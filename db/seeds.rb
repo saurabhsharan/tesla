7f69d3a7fc8a59c23f0bd9287e41e1577577b372
Saurabh Sharan <saurabh.sharan1@gmail.com>
2015-05-10 21:57:09 -0700

    Add small amount of seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Voter.create([
+  {
+    szNameFirst: 'Michael',
+    szNameLast: 'Scott',
+    sGender: 'M',
+    szSitusAddress: '1725 Slough Ave',
+    szSitusCity: 'Scranton',
+    sSitusState: 'PA',
+    sSitusZip: '18501',
+  },
+  {
+    szNameFirst: 'Walter',
+    szNameLast: 'White',
+    sGender: 'M',
+    szSitusAddress: '308 Negra Arroyo Lane',
+    szSitusCity: 'Albuquerque',
+    sSitusState: 'NM',
+    sSitusZip: '87101',
+  },
+  {
+    szNameFirst: 'Sheldon',
+    szNameLast: 'Cooper',
+    sGender: 'M',
+    szSitusAddress: '2311 North Los Robles Ave',
+    szSitusCity: 'Pasadena',
+    sSitusState: 'CA',
+    sSitusZip: '91101',
+  },
+  {
+    szNameFirst: 'Jack',
+    szNameLast: 'Bauer',
+    sGender: 'M',
+    szSitusAddress: '1925 S Bundy Dr',
+    szSitusCity: 'Los Angeles',
+    sSitusState: 'CA',
+    sSitusZip: '90025',
+  },
+  {
+    szNameFirst: 'Ted',
+    szNameLast: 'Mosby',
+    sGender: 'M',
+    szSitusAddress: '150 W. 85th Street',
+    szSitusCity: 'New York',
+    sSitusState: 'NY',
+    sSitusZip: '10024',
+  },
+])
+
+Admin.create([
+  {
+    full_name: 'Admin',
+    email: 'admin@admin.com',
+    password: 'admin',
+  },
+])
